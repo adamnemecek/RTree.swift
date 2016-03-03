49e4bfbc547402d0840682ee055a07fd1f723060
Kai Wells <support@kaiwells.me>
2016-03-03 10:39:13 -0600

    Initial Xcode Commit

---
+++ Example/RTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RTree
+//
+//  Created by Kai Wells on 3/3/16.
+//  Copyright © 2016 Kai Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
